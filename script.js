fa4b262ebd8ac070c2698f560bb90ece304c6a73
saravananravir <168327190+saravananravir@users.noreply.github.com>
2024-05-06 21:38:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let obj1 = { name: 'Person 1', age: 5 };
+let obj2 = { age: 5, name: 'Person 1' };
+
+function areObjectsEqual(obj1, obj2) {
+    const keys1 = Object.keys(obj1);
+    const keys2 = Object.keys(obj2);
+ 
+    if (keys1.length !== keys2.length) {
+        return false;
+    }
+ 
+    for (const key of keys1) {
+        if (obj1[key] !== obj2[key]) {
+            return false;
+        }
+    }
+ 
+    return true;
+}
+console.log(areObjectsEqual(obj1, obj2));
